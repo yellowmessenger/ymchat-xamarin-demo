094e0c95a70e6c078dddb18495f78dad454ebd83
PurushYM <purushottam.yadav@yellowmessenger.com>
2021-09-01 13:58:05 +0530

    Created initial demo application

---
+++ ymchat.demo/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Xamarin.Forms;
+using YmChat;
+
+namespace ymchat.demo
+{
+    public partial class App : Application
+    {
+        public App(IYmChat iymchat)
+        {
+            InitializeComponent();
+            MainPage = new MainPage(iymchat);
+        }
+
+        protected override void OnStart()
+        {
+           
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ymchat.demo/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using YmChat;
+
+namespace ymchat.demo
+{
+    public partial class MainPage : ContentPage
+    {
+        IYmChat _ymChatInterface;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public MainPage(IYmChat ymChatInterface)
+        {
+            Console.WriteLine("Xamarin Forms started");
+            var Payload = new Dictionary<string, object> { { "Name ", "Ym" } };
+
+            _ymChatInterface = ymChatInterface;
+            string botId = "x1597301712805";//bot ID
+            _ymChatInterface.setBotId(botId);//Setting the BOTId
+            _ymChatInterface.setEnableSpeech(true);//Enabling the Speech to text Button
+            _ymChatInterface.setEnableHistory(true);//Enabling Chat History
+            _ymChatInterface.showCloseButton(true);//Showing the Close Button
+            _ymChatInterface.setPayLoad(Payload);//setting the payload 
+            _ymChatInterface.onEventFromBot((botEvent) =>
+            {
+                Console.WriteLine(botEvent["code"]);
+                Console.WriteLine(botEvent["data"]);
+            });
+
+            _ymChatInterface.onBotClose(() =>
+            {
+                Console.WriteLine("Chatbot closed");
+            });
+
+
+            InitializeComponent();
+            
+
+
+        }
+
+        void OnButtonClick(object sender, EventArgs e)
+        {
+            _ymChatInterface.startChatBot();// starting the bot
+        }
+
+
+
+    }
+}
